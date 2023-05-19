93394c785339ff6a5dca8add0aa0dab765197362
bag2000 <118651726+bag2000@users.noreply.github.com>
2023-05-19 20:34:19 +0300

    Update restart.bat

--- restart.bat
+++ restart.bat
@@ -1,3 +1,4 @@
+git clone "https://github.com/bag2000/zabscripts" "C:\zabscripts" 2>nul || git -C "C:\zabscripts" pull || rmdir /S /Q "C:\zabscripts" && git clone "https://github.com/bag2000/zabscripts" "C:\zabscripts"
 net stop "Zabbix Agent"
-timeout 60 > NUL
+timeout 10 > NUL
 net start "Zabbix Agent"
