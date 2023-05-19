00c516b8da42208011dcb87806dd0c8ad0cbbc08
bag2000 <118651726+bag2000@users.noreply.github.com>
2023-05-19 21:53:19 +0300

    Update win_install.bat

--- win_install.bat
+++ win_install.bat
@@ -2,7 +2,4 @@ SCHTASKS /Create /RU SYSTEM /SC ONCE /ST 22:00 /TN RestartZabbix /TR "C:\zabscri
 curl -L https://www.python.org/ftp/python/3.11.3/python-3.11.3-amd64.exe -o "C:\zabscripts\python.exe"
 "C:\zabscripts\python.exe" /quiet Include_debug=0 Include_symbols=0 Shortcuts=0 AssociateFiles=1 Include_launcher=0 Include_tcltk=0 InstallAllUsers=1 PrependPath=1 TargetDir="C:\Program Files\Python3"
 del /Q "C:\zabscripts\python.exe"
-curl -L https://github.com/git-for-windows/git/releases/download/v2.41.0-rc0.windows.1/Git-2.41.0-rc0-64-bit.exe -o "C:\zabscripts\git.exe"
-"C:\zabscripts\git.exe" /VERYSILENT /NORESTART
-del /Q "C:\zabscripts\git.exe"
 SCHTASKS /Run /I /TN RestartZabbix
