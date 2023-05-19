3fefc679d63e64cd4f5deea34a1e118a4a5e9809
bag2000 <118651726+bag2000@users.noreply.github.com>
2023-05-19 20:37:35 +0300

    Update restart.bat

--- restart.bat
+++ restart.bat
@@ -1,4 +1,5 @@
 git clone "https://github.com/bag2000/zabscripts" "C:\zabscripts" 2>nul || git -C "C:\zabscripts" pull || rmdir /S /Q "C:\zabscripts" && git clone "https://github.com/bag2000/zabscripts" "C:\zabscripts"
+copy /Y "C:\zabscripts\UserParametrs.conf" "C:\Program Files\Zabbix Agent\zabbix_agentd.d\UserParametrs.conf"
 net stop "Zabbix Agent"
 timeout 10 > NUL
 net start "Zabbix Agent"
