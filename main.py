9fc96a80b02543b22534e708902919181fa7d0ae
Polyakov Roman <polyakov.r.v@yandex.ru>
2023-05-23 16:24:25 +0300

    Change main.py. add kick, version 1.2.0

--- main.py
+++ main.py
@@ -1,10 +1,12 @@
 import argparse
 from printer import show_text
 
-version = '1.1.1'
+version = '1.2.0'
 
 if __name__ == "__main__":
     parser = argparse.ArgumentParser(description='Скрипты.')
+    parser.add_argument('--kick', nargs='?', default='',
+                        help='Название учетной записи которую необходимо кикнуть')
     parser.add_argument('--reboot', action='store_const', const=version, help='Перезагрузка сервера')
     parser.add_argument('--service', nargs='?', choices=['status', 'start', 'stop', 'restart'], default='',
                         help='Управление службами. Возможные значения: status, start, stop, restart\n'
@@ -34,13 +36,16 @@ if __name__ == "__main__":
     # Перезагрузка сервера
     if args.reboot is not None:
         import sys
-        import subprocess
         import os
         if sys.platform == 'win32':
-            proc = subprocess.Popen(['shutdown', '-g', '-f', '-t', '0'], stdout=subprocess.PIPE, shell=True, encoding='cp866')
-            for line in proc.stdout:
-                print(line)
+            os.popen('shutdown -g -f -t 0')
             show_text('Передан сигнал перезагрузки')
         else:
             os.popen('reboot')
             show_text('Передан сигнал перезагрузки')
+
+    # Кикнуть пользователя
+    if args.kick != '':
+        import os
+        session = os.popen(f'qwinsta | findstr {args.kick}').read()[1:].split()[0]
+        kick = os.popen(f'rwinsta {session} /server:localhost')
